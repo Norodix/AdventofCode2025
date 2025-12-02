86eedbe2674654abd5e33e08edc91b282926525b
Norodix <peceli.bence@gmail.com>
2025-12-02 20:34:31 +0100

    Day 2 part 2

--- day_02/src/main.rs
+++ day_02/src/main.rs
@@ -16,12 +16,34 @@ fn num_digits(number: u64) -> u64 {
 }
 
 fn check_repeat(number: u64) -> bool {
+    check_repeat_n(number, 2)
+}
+
+fn check_repeat_all(number: u64) -> bool {
     let n_size = num_digits(number);
-    if n_size % 2 != 0 {
+    for i in 2..=n_size {
+        if check_repeat_n(number, i) {
+            return true;
+        }
+    }
+    false
+}
+
+// Check if there is a repeating pattern n times
+fn check_repeat_n(number: u64, n: u64) -> bool {
+    let n_size = num_digits(number);
+    if n_size % n != 0 {
         return false;
     }
-    let div = 10u64.pow((n_size / 2) as u32);
-    number / div == number % div
+    let pattern_len = n_size / n;
+    let div = 10u64.pow(pattern_len as u32);
+    for index in 1..n {
+        let digs = (number / div.pow(index as u32)) % div;
+        if digs != number % div {
+            return false;
+        }
+    }
+    return true;
 }
 
 fn solve(filepath: &str) -> u64 {
@@ -43,7 +65,19 @@ fn solve(filepath: &str) -> u64 {
 
 fn solve2(filepath: &str) -> u64 {
     let f = std::fs::read_to_string(filepath).expect("File could not be read");
-    0
+    let mut sum = 0;
+    let ranges = f.split(",");
+    for range in ranges {
+        let mut r = range.split("-");
+        let lower: u64 = r.next().unwrap().trim().parse().unwrap();
+        let higher: u64 = r.next().unwrap().trim().parse().unwrap();
+        for id in lower..=higher {
+            if check_repeat_all(id) {
+                sum += id;
+            }
+        }
+    }
+    sum
 }
 
 #[cfg(test)]
@@ -66,8 +100,31 @@ mod tests {
         assert_eq!(check_repeat(123123), true);
     }
 
-    // #[test]
-    // fn example2() {
-    //     let eg = solve2("example");
-    // }
+    #[test]
+    fn test_repeat_n_check() {
+        assert_eq!(check_repeat_n(111, 3), true);
+        assert_eq!(check_repeat_n(111, 2), false);
+        assert_eq!(check_repeat_n(101, 3), false);
+        assert_eq!(check_repeat_n(6464, 2), true);
+        assert_eq!(check_repeat_n(123123123, 2), false);
+    }
+
+    #[test]
+    fn test_repeat_all_check() {
+        assert_eq!(check_repeat_all(111), true);
+        assert_eq!(check_repeat_all(11), true);
+        assert_eq!(check_repeat_all(121212), true);
+        assert_eq!(check_repeat_all(123123123), true);
+        assert_eq!(check_repeat_all(9090), true);
+        assert_eq!(check_repeat_all(101), false);
+        assert_eq!(check_repeat_all(10), false);
+        assert_eq!(check_repeat_all(121112), false);
+        assert_eq!(check_repeat_all(123113123), false);
+    }
+
+    #[test]
+    fn example2() {
+        let eg = solve2("example");
+        assert_eq!(eg, 4174379265);
+    }
 }
