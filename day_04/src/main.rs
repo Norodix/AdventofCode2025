e10a6042173f5d93feae0e9cc51565c1f281b58b
Norodix <peceli.bence@gmail.com>
2025-12-04 07:38:48 +0100

    Day 4 part 2

--- day_04/src/main.rs
+++ day_04/src/main.rs
@@ -10,6 +10,54 @@ fn print_tiles(tiles: &Vec<char>, cols: usize) {
         }
         print!("\n");
     }
+    print!("\n");
+}
+
+fn cleanup(tiles: &mut Vec<char>, cols: usize) -> u32 {
+    let mut counts = vec![0; tiles.len()];
+    let cols = cols as i32;
+    let rows = tiles.len() as i32 / cols;
+
+    let mut valid = 0;
+    for t in 0..tiles.len() {
+        if tiles[t] != '@' {
+            continue;
+        }
+        let mut cnt = 0;
+        for r in -1i32..=1 {
+            for c in -1i32..=1 {
+                let t = t as i32;
+                let row = t / cols;
+                if row + r < 0 || row + r >= rows {
+                    continue;
+                }
+                let col = t % cols;
+                if col + c < 0 || col + c >= cols {
+                    continue;
+                }
+                let index = t + r * cols + c;
+                if index < 0 || index >= tiles.len() as i32 {
+                    continue;
+                }
+                let index = index as usize;
+                if tiles[index] == '@' {
+                    cnt += 1;
+                }
+            }
+        }
+        counts[t] = cnt;
+        // Fewer than four -> counting itself <= 4
+        if cnt <= 4 {
+            valid += 1;
+            // println!("Valid at {},{}", t / cols as usize, t % cols as usize);
+        }
+    }
+    for t in 0..tiles.len() {
+        if counts[t] <= 4 && counts[t] > 0 {
+            tiles[t] = '.';
+        }
+    }
+    valid
 }
 
 fn solve(filepath: &str) -> u32 {
@@ -17,7 +65,7 @@ fn solve(filepath: &str) -> u32 {
     let cols = f.find('\n').unwrap() as i32;
     let tiles: Vec<char> = f.chars().filter(|x| *x != '\n').collect();
     let rows = (tiles.len() as i32) / cols;
-    // print_tiles(&tiles, cols as usize);
+    print_tiles(&tiles, cols as usize);
     let mut counts = tiles.clone();
 
     let mut valid = 0;
@@ -59,7 +107,19 @@ fn solve(filepath: &str) -> u32 {
 }
 
 fn solve2(filepath: &str) -> u32 {
-    0
+    let f = std::fs::read_to_string(filepath).expect("File could not be read");
+    let cols = f.find('\n').unwrap();
+    let mut tiles: Vec<char> = f.chars().filter(|x| *x != '\n').collect();
+    let mut sum = 0;
+
+    let mut valid = cleanup(&mut tiles, cols);
+    while valid > 0 {
+        print_tiles(&tiles, cols);
+        sum += valid;
+        valid = cleanup(&mut tiles, cols);
+    }
+
+    sum
 }
 
 #[cfg(test)]
@@ -67,7 +127,12 @@ mod tests {
     use super::*;
 
     #[test]
-    fn example2() {
+    fn example() {
         assert_eq!(solve("example"), 13);
+    }
+
+    #[test]
+    fn example2() {
+        assert_eq!(solve2("example"), 43);
     }
 }
