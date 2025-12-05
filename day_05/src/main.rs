d0eca0a7889a04b51d5afd3e13e7f454b13f6491
Norodix <peceli.bence@gmail.com>
2025-12-05 17:38:28 +0100

    Day 5 part 2

--- day_05/src/main.rs
+++ day_05/src/main.rs
@@ -17,6 +17,24 @@ fn is_fresh(range: &Vec<NumRange>, n: u64) -> bool {
     return false;
 }
 
+fn merge_ranges(a: &NumRange, b: &NumRange) -> NumRange {
+    NumRange {
+        min: std::cmp::min(a.min, b.min),
+        max: std::cmp::max(a.max, b.max),
+    }
+}
+
+fn is_overlapping(a: &NumRange, b: &NumRange) -> bool {
+    if a.min <= b.max && a.min >= b.min
+        || a.max <= b.max && a.max >= b.min
+        || b.min <= a.max && b.min >= a.min
+        || b.max <= a.max && b.max >= a.min
+    {
+        return true;
+    }
+    false
+}
+
 fn solve(filepath: &str) -> u32 {
     let f = std::fs::read_to_string(filepath).expect("File could not be read");
     let mut parts = f.split("\n\n");
@@ -41,8 +59,55 @@ fn solve(filepath: &str) -> u32 {
     cnt
 }
 
-fn solve2(filepath: &str) -> u32 {
-    0
+fn print_ranges(v: &Vec<NumRange>) {
+    for v in v {
+        println!("{}-{}", v.min, v.max);
+    }
+}
+
+fn solve2(filepath: &str) -> u64 {
+    let f = std::fs::read_to_string(filepath).expect("File could not be read");
+    let mut parts = f.split("\n\n");
+    let mut numranges: Vec<NumRange> = vec![];
+    let ranges = parts.next().unwrap().split('\n');
+    for r in ranges {
+        let mut minmax = r.split('-');
+        let range = NumRange {
+            min: minmax.next().unwrap().parse().unwrap(),
+            max: minmax.next().unwrap().parse().unwrap(),
+        };
+        numranges.push(range);
+    }
+    // println!("Before");
+    // print_ranges(&numranges);
+    // Check for overlapping regions, replace them with merged
+    let mut i = 0;
+    let mut j = 0;
+    'outer: while i < numranges.len() - 1 {
+        j = i + 1;
+        while j < numranges.len() {
+            if is_overlapping(&numranges[i], &numranges[j]) {
+                // println!(
+                //     "{}-{} x {}-{}",
+                //     numranges[i].min, numranges[i].max, numranges[j].min, numranges[j].max
+                // );
+                numranges.push(merge_ranges(&numranges[i], &numranges[j]));
+                numranges.remove(j);
+                numranges.remove(i);
+                i = 0;
+                j = 0;
+                continue 'outer;
+            }
+            j += 1;
+        }
+        i += 1;
+    }
+
+    let mut sum: u64 = 0;
+    for r in numranges {
+        sum += r.max - r.min + 1;
+    }
+    sum
 }
 
 #[cfg(test)]
@@ -56,6 +121,6 @@ mod tests {
 
     #[test]
     fn example2() {
-        assert_eq!(solve2("example"), 43);
+        assert_eq!(solve2("example"), 14);
     }
 }
