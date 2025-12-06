7bfa0a425d14c1548b2f697fe163aaf68ac7fbe8
Norodix <peceli.bence@gmail.com>
2025-12-06 10:49:44 +0100

    Day 6 part 2

--- day_06/src/main.rs
+++ day_06/src/main.rs
@@ -48,7 +48,48 @@ fn solve(filepath: &str) -> u64 {
 }
 
 fn solve2(filepath: &str) -> u64 {
-    0
+    let f = std::fs::read_to_string(filepath).expect("File could not be read");
+    let operators: Vec<char> = f
+        .lines()
+        .last()
+        .unwrap()
+        .chars()
+        .filter(|c| !c.is_whitespace())
+        .collect();
+    let rows = f.lines().count(); // including the operator
+    let f: Vec<char> = f.chars().collect();
+    let mut nums: Vec<u64> = vec![];
+    let cols = f.len() / rows;
+    let mut op_index = 0;
+    let mut sum = 0;
+    for c in 0..cols {
+        let mut s = String::from("");
+        for r in 0..(rows - 1) {
+            s.push(f[r * cols + c]);
+        }
+        let s = s.trim();
+        if !s.is_empty() {
+            nums.push(s.parse().unwrap());
+        } else {
+            // consume the numbers collected in nums so far
+            sum += nums
+                .iter()
+                .copied()
+                .reduce(|acc, e| {
+                    if operators[op_index] == '+' {
+                        acc + e
+                    } else {
+                        acc * e
+                    }
+                })
+                .unwrap();
+            nums.clear();
+            // Increment operator index
+            op_index += 1;
+        }
+    }
+
+    sum
 }
 
 #[cfg(test)]
@@ -58,5 +99,10 @@ mod tests {
     #[test]
     fn example() {
         assert_eq!(solve("example"), 4277556);
+    }
+
+    #[test]
+    fn example2() {
+        assert_eq!(solve2("example"), 3263827);
     }
 }
