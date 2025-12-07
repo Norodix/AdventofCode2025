846d625f56da635c916eee856151fa9db2bbc84d
Norodix <peceli.bence@gmail.com>
2025-12-07 08:29:49 +0100

    Day 7 part 2

--- day_07/src/main.rs
+++ day_07/src/main.rs
@@ -27,8 +27,65 @@ fn solve(filepath: &str) -> u64 {
     splits
 }
 
+// Part 2 Algorithm:
+// Loop through beam splitters line by line
+// Each beam splitter looks up and sums up all the beams above it until the next splitter
+// the sum is added to its left and right neighbors
+// At the end, sum up all the beams up to the nearest beam splitter for each column
+enum Tile {
+    Count(u64),
+    Splitter,
+}
+
+fn sum_above(tiles: &Vec<Tile>, cols: usize, i: usize) -> u64 {
+    let mut sum = 0;
+    let mut i = i as i64;
+    let cols = cols as i64;
+    i -= cols; // start 1 tile above
+    while i > 0 {
+        match tiles[i as usize] {
+            Tile::Splitter => break,
+            Tile::Count(x) => sum += x,
+        }
+        i -= cols;
+    }
+    sum
+}
+
 fn solve2(filepath: &str) -> u64 {
-    0
+    let f = std::fs::read_to_string(filepath).expect("File could not be read");
+    let cols = f.chars().position(|x| x == '\n').unwrap();
+    let mut tiles: Vec<Tile> = f
+        .chars()
+        .filter(|x| *x != '\n')
+        .map(|x| match x {
+            '.' => Tile::Count(0),
+            'S' => Tile::Count(1),
+            '^' => Tile::Splitter,
+            _ => panic!("Unexpected char found: {x}"),
+        })
+        .collect();
+    for i in cols..tiles.len() {
+        match tiles[i] {
+            Tile::Splitter => {
+                let s = sum_above(&tiles, cols, i);
+                if let Tile::Count(c) = tiles[i - 1] {
+                    tiles[i - 1] = Tile::Count(c + s);
+                }
+                if let Tile::Count(c) = tiles[i + 1] {
+                    tiles[i + 1] = Tile::Count(c + s);
+                }
+            }
+            _ => (),
+        }
+    }
+
+    // go through the last row and sum everything above it
+    let mut sum = 0;
+    for i in (tiles.len() - cols)..tiles.len() {
+        sum += sum_above(&tiles, cols, i);
+    }
+    sum
 }
 
 #[cfg(test)]
@@ -42,6 +99,6 @@ mod tests {
 
     #[test]
     fn example2() {
-        assert_eq!(solve2("example"), 0);
+        assert_eq!(solve2("example"), 40);
     }
 }
