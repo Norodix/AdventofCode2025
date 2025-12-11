2035177b17b8b1f6049f3296be82b4e8408b3b34
Norodix <peceli.bence@gmail.com>
2025-12-11 20:08:45 +0100

    Day 11 part 2

--- day_11/src/main.rs
+++ day_11/src/main.rs
@@ -7,17 +7,17 @@ fn main() {
     println!("{}", solve("input"));
     let elapsed = now.elapsed();
     println!("Part 1 time: {}us", elapsed.as_micros());
-    // let now = std::time::Instant::now();
-    // println!("{}", solve2("input"));
-    // let elapsed = now.elapsed();
-    // println!("Total time to compute: {}us", elapsed.as_micros());
+    let now = std::time::Instant::now();
+    println!("{}", solve2("input"));
+    let elapsed = now.elapsed();
+    println!("Total time to compute: {}us", elapsed.as_micros());
 }
 
 #[derive(Debug)]
 struct Device {
     id: String,
     next: Vec<String>,
-    value: u64,
+    value: i64,
 }
 
 fn ways_to_id(from: &str, to: &str, hm: &HashMap<&str, Device>) -> u64 {
@@ -30,6 +30,41 @@ fn ways_to_id(from: &str, to: &str, hm: &HashMap<&str, Device>) -> u64 {
         let next = &d.next[n];
         sum += ways_to_id(&next, to, hm);
     }
+    sum
+}
+
+fn clear_values(hm: &mut HashMap<&str, Device>) {
+    let mut keys: Vec<&str> = vec![];
+    for k in hm.keys() {
+        keys.push(k);
+    }
+    for k in keys {
+        hm.get_mut(k).unwrap().value = -1;
+    }
+}
+
+fn ways_to_id2(from: &str, to: &str, hm: &mut HashMap<&str, Device>) -> i64 {
+    if from == to {
+        return 1;
+    }
+    let d = hm.get(from).expect(&format!("Unexpected id found: {from}"));
+    if d.value != -1 {
+        return d.value;
+    }
+    // // special case
+    // if d.id == "out" {
+    //     return 0;
+    // }
+    let mut sum = 0;
+    let vec = d.next.clone();
+    for n in vec {
+        sum += ways_to_id2(&n, to, hm);
+    }
+
+    let d = hm
+        .get_mut(from)
+        .expect(&format!("Unexpected id found: {from}"));
+    d.value = sum;
     sum
 }
 
@@ -59,6 +94,49 @@ fn solve(filepath: &str) -> u64 {
     ways_to_id("you", "out", &hm)
 }
 
+fn solve2(filepath: &str) -> i64 {
+    // read lines into vector of nodes
+    let f = std::fs::read_to_string(filepath).expect("File could not be read");
+    let mut hm: HashMap<&str, Device> = HashMap::new();
+    for l in f.lines() {
+        let id = l.split(':').next().unwrap();
+        let mut d = Device {
+            id: String::from(id),
+            next: vec![],
+            value: -1,
+        };
+        let mut connections = l.split(' ');
+        connections.next();
+        let mut op: Option<&str> = connections.next();
+        while op.is_some() {
+            let n = op.unwrap();
+            // enable to help dot file graph generation
+            // println!("{id} -> {n}");
+            d.next.push(String::from(op.unwrap()));
+            op = connections.next();
+        }
+        hm.insert(id, d);
+    }
+    // Special device that doesn't lead anywhere
+    let d = Device {
+        id: String::from("out"),
+        next: vec![],
+        value: -1,
+    };
+    hm.insert("out", d);
+
+    // Order is svr -> fft -> dac -> out
+    let svr_fft = ways_to_id2("svr", "fft", &mut hm);
+    println!("{svr_fft}");
+    clear_values(&mut hm);
+    let fft_dac = ways_to_id2("fft", "dac", &mut hm);
+    println!("{fft_dac}");
+    clear_values(&mut hm);
+    let dac_out = ways_to_id2("dac", "out", &mut hm);
+    println!("{dac_out}");
+    svr_fft * fft_dac * dac_out
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -66,5 +144,10 @@ mod tests {
     #[test]
     fn example() {
         assert_eq!(solve("example"), 5);
+    }
+
+    #[test]
+    fn example2() {
+        assert_eq!(solve2("example2"), 2);
     }
 }
